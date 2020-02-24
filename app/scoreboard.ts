1f4ac8e7162a1a881aeab07baab0ce54193833de
martinash78 <martin.ashcroft1978@gmail.comj>
2020-02-24 17:37:22 +0000

    Working app without using modules

---
+++ app/scoreboard.ts
@@ -0,0 +1,26 @@
+import { Result } from './result';
+
+export class Scoreboard {
+
+    private results: Result[] = [];
+
+    addResult(newResult: Result): void {
+        this.results.push(newResult);
+    }
+
+    updateScoreboard(): void {
+        let output: string = '<h2>Scoreboard</h2>';
+        console.log(this.results);
+        console.log('len=' + this.results.length);
+        for (let index=0; index < this.results.length; index++) {
+            const result: Result = this.results[index];
+            console.log('Looping ' + result);
+            output+= '<h4>';
+            output += result.playerName + ': ' + result.score + '/' + result.problemCount + ' for factor ' + result.factor;
+            output += '</h4>';     
+        }
+        
+        const scoresElement: HTMLElement = document.getElementById('scores')!;
+        scoresElement.innerHTML = output;   
+    }
+}
